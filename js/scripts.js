8d01fe229591339e25ea548f33f8534bd51ce859
mihidin <amirovm38@gmail.com>
2024-06-13 22:21:21 +0600

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,4 @@
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Спасибо за ваше сообщение! Мы свяжемся с вами в ближайшее время.');
+});
